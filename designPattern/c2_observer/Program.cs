f2a6685c27bfc08dc9da1b7adfd265f739c094e9
tong <paul.tong.cs@gmail.com>
2020-01-27 17:46:51 -0800

    create observer folder

---
+++ designPattern/c2_observer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+NOTE: The Observer Pattern defines a one-to-many dependency between objects so that when one
+object changes state, all of its dependents are notified and updated automatically
+real life example:
+news subscriber(observer) get nodification from publisher(subject) when news come out
+*/
+namespace c2_observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

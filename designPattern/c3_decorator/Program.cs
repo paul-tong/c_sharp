89bb9267394d0d07ebf9e4f3e8aa8a68f7006dff
tong <paul.tong.cs@gmail.com>
2020-01-29 15:59:40 -0800

    finish decorator with abstract class

--- designPattern/c3_decorator/Program.cs
+++ designPattern/c3_decorator/Program.cs
@@ -17,14 +17,14 @@ decorates to do the rest of the job.
 - Objects can be decorated at any time, so we can decorate objects dynamically at runtime
 with as many decorators as we like.
 
-- extends/impelements from main class -> ensure to have same class type, will have same methods as main class
-- has an instance (eg, member) of main class as data member -> can get behaviors from composition (like recursion, 套娃)
-- call the bahavior functions of data member(decorated class), decorate it with some new behaviors
+- decorator extends/impelements from main class(decrorated class) -> ensure to have same class type, will have same methods as main class
+- decorator has an instance (eg, member) of main class as data member -> can get behaviors from composition (like recursion, 套娃)
+- decorator calls the bahavior functions of data member(decorated class), then decorate it with some new behaviors
     eg, testFun(){member.testFun() + some new behaviors} 
 - can decorate class with addition new behaviors(methods)
 
 Pros:
-- reating flexible designs and staying true to the Open-Closed Principle
+- reating flexible designs and staying true to the Open-Closed Principle (open to extension, close to change existed codes)
 - you can usually insert decorators transparently and 
     the client never has to know it’s dealing with a decorator
 
@@ -38,24 +38,80 @@ Java I/O classes: eg, FileInputStream, BudfferedInputStream, LineNumberInputStre
 */
 namespace c3_decorator
 {
+    public abstract class Beverage {
+        protected string description = "Unkown Beverage";
+
+        public string GetDescription() {
+            return description;
+        }
+
+        public abstract double GetCost();
+    }
+
+    public class Espresso: Beverage {
+        public Espresso () {
+            description = "Espresso";
+        }
+
+        public override double GetCost() {
+            return 1.99;
+        }
+    }
+
+    public class Latte: Beverage {
+        public Latte() {
+            description = "Latte";
+        }
+
+        public override double GetCost() {
+            return 0.89;
+        }
+    }
+
+    public abstract class CondimentDecorator: Beverage {
+        public new abstract string GetDescription();
+    }
+
+
+    public class Mocha: CondimentDecorator {
+        protected Beverage beverage;
+
+        public Mocha(Beverage beverage) {
+            this.beverage = beverage;
+        }
+
+        public override string GetDescription() {
+            return beverage.GetDescription() + ", Mocha";
+        }
+
+        public override double GetCost() {
+            return beverage.GetCost() + 0.20;
+        }
+    }
+
+    public class Chocolate: CondimentDecorator {
+        protected Beverage beverage;
+
+        public Chocolate(Beverage beverage) {
+            this.beverage = beverage;
+        }
+
+        public override string GetDescription() {
+            return beverage.GetDescription() + ", Chocolate";
+        }
+
+        public override double GetCost() {
+            return beverage.GetCost() + 0.15;
+        }
+    }
+
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-        }
-
-        public void test1() {
-
-        }
-
-        public void test() {
-            
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
+            Beverage b1 = new Espresso();
+            Console.WriteLine($"beverage: {b1.GetDescription()}, ${b1.GetCost()}");
         }
     }
 }
